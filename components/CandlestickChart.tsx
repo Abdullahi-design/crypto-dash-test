c3d0cb17cc7df8104e7aafe5e7fb1480d0389cc1
Abdullahi-design <abdullahismohd3000@gmail.com>
2024-12-12 11:09:34 +0100

    react-ts-tradingview-widgets

--- components/CandlestickChart.tsx
+++ components/CandlestickChart.tsx
@@ -1,7 +1,21 @@
-export const CandlestickChart = () => (
-    <div className="bg-[#20252B] rounded-xl p-4 h-full">
-      <div className="text-center text-sm mb-2">Candlestick Chart</div>
-      <div className="h-80 bg-gray-700">[Chart Placeholder]</div>
+"use client";
+
+import { AdvancedRealTimeChart } from "react-ts-tradingview-widgets";
+
+const CandlestickChart = () => {
+  return (
+    <div className="bg-[#20252B] rounded-lg p-4">
+      <div className="h-[35rem] w-full">
+        <AdvancedRealTimeChart
+          symbol="BTCUSDT"
+          theme="dark"
+          autosize
+          hide_top_toolbar={false}  // Hide the top toolbar (including tabs)
+          hide_side_toolbar={true} // Optionally hide the side toolbar
+        />
+      </div>
     </div>
-);
-  
+  );
+};
+
+export default CandlestickChart;
